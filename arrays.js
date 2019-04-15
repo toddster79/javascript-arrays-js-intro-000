e5ebb36432991653cd0819563b76bd1552846b35
Todd Carlson <todd.carlson3030@me.com>
2019-04-15 07:14:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length -1);
-  return array;
+  var newArray = array.slice(0, array.length -1);
+  return newArray;
 }
 
